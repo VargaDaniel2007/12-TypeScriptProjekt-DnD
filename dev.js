74877b792dc79e6b0285c71cbf2fd176ada69705
Varga Dániel <varga.daniel@students.jedlik.eu>
2026-05-21 14:00:09 +0200

    "npm run dev" mostmár elindítja a vite-t és a json-servert

---
+++ dev.js
@@ -0,0 +1,15 @@
+import { spawn } from 'child_process';
+// const { spawn } = require ('child_process');
+
+const startProcess = (command, args) => {
+	const proc = spawn(command, args, {stdio: 'inherit', shell: true});
+	proc.on('close', () => process.exit());
+};
+
+startProcess('json-server', ['--watch', 'db.json', '--port', '3241']);
+startProcess('vite', ['--port', '5684']);
+// "scripts": {
+//   "dev": "vite --port 5684",
+//   "build": "tsc && vite build",
+//   "preview": "vite preview"
+// },
